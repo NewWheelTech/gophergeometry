56caef7c0660c49e1b12344d7a924b0d9eb6ea25
NewWheelTech <97138790+NewWheelTech@users.noreply.github.com>
2023-05-26 14:55:34 -0400

    Update date and website

--- Gopher/GopherProjectSelectedFaces.cs
+++ Gopher/GopherProjectSelectedFaces.cs
@@ -1,6 +1,6 @@
 ﻿///////////////////////////////////////////////////////////////////////////////
 // Gopher Geometry
-// Copyright(C) 2022 Matthew Newberg
+// Copyright(C) 2023 Matthew Newberg
 
 //Permission is hereby granted, free of charge, to any person obtaining a copy
 //of this software and associated documentation files (the "Software"), to deal
--- Gopher/Properties/AssemblyInfo.cs
+++ Gopher/Properties/AssemblyInfo.cs
@@ -8,12 +8,12 @@ using Rhino.PlugIns;
 // These will show in Rhino's option dialog, in the tab Plug-ins
 [assembly: PlugInDescription(DescriptionType.Address, "-")]
 [assembly: PlugInDescription(DescriptionType.Country, "-")]
-[assembly: PlugInDescription(DescriptionType.Email, "mnewberg@gmail.com")]
+[assembly: PlugInDescription(DescriptionType.Email, "mnewberg@newwheeltechnology.com")]
 [assembly: PlugInDescription(DescriptionType.Phone, "-")]
 [assembly: PlugInDescription(DescriptionType.Fax, "-")]
 [assembly: PlugInDescription(DescriptionType.Organization, "-")]
 [assembly: PlugInDescription(DescriptionType.UpdateUrl, "-")]
-[assembly: PlugInDescription(DescriptionType.WebSite, "http://mattnewberg.info/Gopher/")]
+[assembly: PlugInDescription(DescriptionType.WebSite, "http://newwheeltechnology.com/")]
 
 
 // General Information about an assembly is controlled through the following 
